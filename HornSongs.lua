7638bb01b80fdca8bc0d6b975e2333e339d4a790
Tyler Chamberlain <hexarobi@gmail.com>
2022-09-03 16:02:00 -0500

    Update HornSongs.lua

--- HornSongs.lua
+++ HornSongs.lua
@@ -1,10 +1,11 @@
--- HornSongs v1.0
+-- HornSongs v1.1
 -- by Hexarobi
 -- Install in `Stand/Lua Scripts`
 
 util.require_natives(1660775568)
 
 local pause=0
+
 local do1=16
 local re=17
 local mi=18
@@ -14,17 +15,32 @@ local la=21
 local ti=22
 local do2=23
 
+local C1 = 16
+local D1 = 17
+local E1 = 18
+local F1 = 19
+local G1 = 20
+local A1 = 21
+local B1 = 22
+local C2 = 23
+
 local whole = 1
 local half = 2
 local quarter = 4
 local eighth = 8
 local sixteenth = 16
 
+local MOD_HORN = 14
+
+local horn_on = false
+
 local songs = {
     {
+        -- From https://www.easymusicnotes.com/pdf-piano-1/twinkle-twinkle-little-star-classical-mozart-piano-level-1.pdf
         name = "Twinkle Twinkle Little Star",
-        bpm = 120,
+        bpm = 90,
         notes = {
+            pause,
             do1, do1, sol, sol, la, la, { pitch = sol, length = half }, pause,
             fa, fa, mi, mi, re, re, { pitch = do1, length = half }, pause,
             sol, sol, fa, fa, mi, mi, { pitch = re, length = half }, pause,
@@ -32,73 +48,88 @@ local songs = {
             do1, do1, sol, sol, la, la, { pitch = sol, length = half }, pause,
             fa, fa, mi, mi, re, re, { pitch = do1, length = half }, pause,
         },
-    }
+    },
+    {
+        -- From https://www.easymusicnotes.com/pdf-piano-1/au-claire-de-la-lune-children-traditional-piano-level-1.pdf
+        name = "Au Claire De La Lune",
+        bpm = 60,
+        notes = {
+            fa, fa, fa, sol, { pitch = la, length = half }, { pitch = sol, length = half },
+            fa, la, sol, sol, { pitch = fa, length = whole },
+            fa, fa, fa, sol,  { pitch = la, length = half }, { pitch = sol, length = half },
+            fa, la, sol, sol, { pitch = fa, length = whole },
+        },
+    },
+    {
+        -- From https://www.easymusicnotes.com/pdf-piano-1/hot-cross-buns-children-traditional-piano-level-1.pdf
+        name = "Hot Cross Buns",
+        bpm = 60,
+        notes = {
+            { pitch = mi, length = half }, { pitch = re, length = half },
+            { pitch = do1, length = half }, { pitch = pause, length = half },
+            { pitch = mi, length = half }, { pitch = re, length = half },
+            { pitch = do1, length = half }, { pitch = pause, length = half },
+            do1, do1, do1, do1, re, re, re, re,
+            { pitch = mi, length = half }, { pitch = re, length = half },
+            { pitch = do1, length = half }, { pitch = pause, length = half },
+        },
+    },
 }
 
-local function load_hash(hash)
-    STREAMING.REQUEST_MODEL(hash)
-    while not STREAMING.HAS_MODEL_LOADED(hash) do
-        util.yield()
+local function get_note(note)
+    if type(note) ~= "table" then
+        note = {pitch=note}
     end
+    if note.length == nil then
+        note.length = quarter
+    end
+    return note
 end
 
-local function spawn_vehicle_for_player(pid, model_name)
-    local model = util.joaat(model_name)
-    if STREAMING.IS_MODEL_VALID(model) and STREAMING.IS_MODEL_A_VEHICLE(model) then
-        load_hash(model)
-        local target_ped = PLAYER.GET_PLAYER_PED_SCRIPT_INDEX(pid)
-        local pos = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(target_ped, 0.0, 4.0, 0.5)
-        local heading = ENTITY.GET_ENTITY_HEADING(target_ped)
-        local vehicle = entities.create_vehicle(model, pos, heading)
-        STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(model)
-        return vehicle
+local function play_note(vehicle, song, note, index)
+    note = get_note(note)
+    local note_playtime = math.floor(song.beat_length / note.length)
+    if note.pitch ~= pause then
+        horn_on = true
+        --VEHICLE.START_VEHICLE_HORN(vehicle, note_delay, util.joaat("HELDDOWN"), false)
+    end
+    util.yield(note_playtime)
+    horn_on = false
+    -- Que up pitch for next note
+    if song.notes[index+1] ~= nil then
+        local next_note = get_note(song.notes[index+1])
+        if next_note.pitch ~= pause then
+            VEHICLE.SET_VEHICLE_MOD(vehicle, MOD_HORN, next_note.pitch)
+        end
+    end
+    util.yield(song.beat_length - note_playtime)
+    end
+
+local function play_song(song)
+    song.beat_length = math.floor(60000 / song.bpm)
+    if not PED.IS_PED_IN_ANY_VEHICLE(PLAYER.PLAYER_PED_ID(), true) then
+        util.toast("Cannot play horn unless within a vehicle")
+        return
+    end
+    local vehicle = PED.GET_VEHICLE_PED_IS_IN(PLAYER.PLAYER_PED_ID(), false)
+    if vehicle then
+        local original_horn = VEHICLE.GET_VEHICLE_MOD(vehicle, MOD_HORN)
+        for index, note in pairs(song.notes) do
+            play_note(vehicle, song, note, index)
+        end
+        VEHICLE.SET_VEHICLE_MOD(vehicle, MOD_HORN, original_horn)
     end
 end
 
 for _, song in pairs(songs) do
     menu.action(menu.my_root(), "Play "..song.name, {}, "Spawns a car and plays song on its horn.", function()
-        local vehicle
-        local spawned_vehicle = false
-        --vehicle = entities.get_user_vehicle_as_handle()
-        if not vehicle then
-            vehicle = spawn_vehicle_for_player(players.user(), "kuruma2")
-            spawned_vehicle = true
-        end
-
-        local ped_hash = util.joaat("s_m_m_pilot_01")
-        load_hash(ped_hash)
-        local pos = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(vehicle, 0, 0, 0)
-        local ped = entities.create_ped(1, ped_hash, pos, 0.0)
-        PED.SET_PED_INTO_VEHICLE(ped, vehicle, -1)
-
-        ENTITY.FREEZE_ENTITY_POSITION(vehicle, true)
-
-        local song_beat_length = math.floor((60 / song.bpm) * 1000)
-        if vehicle then
-            for _, note in pairs(song.notes) do
-                if type(note) ~= "table" then
-                    note = {pitch=note}
-                end
-                if note.length == nil then
-                    note.length = quarter
-                end
-                local note_delay = math.floor(song_beat_length / note.length)
-                if note.pitch ~= pause then
-                    VEHICLE.SET_VEHICLE_MOD(vehicle, 14, note.pitch)
-                    --AUDIO.SET_HORN_PERMANENTLY_ON_TIME(vehicle, note_delay)
-                    VEHICLE.START_VEHICLE_HORN(vehicle, note_delay, util.joaat("HELDDOWN"), false)
-                end
-                util.yield(note_delay)
-            end
-
-            if spawned_vehicle then
-                entities.delete_by_handle(ped)
-                entities.delete_by_handle(vehicle)
-            end
-        end
+        play_song(song)
     end)
 end
 
 util.create_tick_handler(function()
+    if horn_on then
+        PAD._SET_CONTROL_NORMAL(0, 86, 1)
+    end
     return true
 end)
